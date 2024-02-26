2c4b8f5b3fb62e5105f893e1fde89b9788e6f8f5
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2024-02-26 11:24:12 -0800

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import '@/assets/styles/globals.css'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+
+export const metadata = {
+  title: 'Property Pulse | Find the perfect rental',
+  description: 'Find your dream rental property',
+  keywords: 'rental, find rentals, find properties',
+
+}
+
+const MainLayout
+ = ({ children }) => {
+  return (
+    <html lang='en'>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  )
+}
+
+export default MainLayout
